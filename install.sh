2ba653cf922c244d4effd24c6ad807691f65bd37
jaraim <117310975+jaraim@users.noreply.github.com>
2023-03-30 12:45:14 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,3 +15,32 @@ bash <(curl -s -L https://raw.githubusercontent.com/jaraim/v2ray/master/go.sh)
 
 # 安装x-ui
 bash <(curl -s -L https://raw.githubusercontent.com/jaraim/xray-x-ui/main/install.sh)
+
+# 安装nginx并配置反向代理
+if [ -d "/etc/nginx/" ]; then # 判断是否存在 Nginx 目录
+    sudo apt-get install nginx -y
+
+    # 配置 Nginx 反向代理
+    echo -n "请输入反向代理服务器的域名或 IP 地址（留空将使用服务器公网 IP 地址）: "
+    read domain
+    if [[ -z "$domain" ]]; then
+        domain=$(curl -s ipv4.icanhazip.com)
+    fi
+    sudo rm /etc/nginx/sites-enabled/default
+    cat << EOF | sudo tee /etc/nginx/sites-available/x-ui.conf
+    server {
+        listen 80;
+        server_name $domain;
+        location / {
+            proxy_pass http://127.0.0.1:65432;
+            proxy_set_header Host $host;
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        }
+    }
+EOF
+    sudo ln -s /etc/nginx/sites-available/x-ui.conf /etc/nginx/sites-enabled/
+    sudo nginx -t && sudo nginx -s reload
+fi
+
+echo "安装完成！"

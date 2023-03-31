50918059817d87fa20da3a5ea044dae8b26fe3e2
jaraim <117310975+jaraim@users.noreply.github.com>
2023-03-31 15:53:39 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
-# 安装依赖工具
 if [[ $(command -v apt-get) ]]; then
 sudo apt-get install curl -y
-   
 elif [[ $(command -v yum) ]]; then
 sudo yum install curl -y
-   
 elif [[ $(command -v dnf) ]]; then
 sudo dnf install curl -y
-    
 else
     echo "不支持的操作系统" && exit 1
 fi
+# 安装依赖工具
 if [[ $(command -v apt-get) ]]; then
-
-    sudo apt-get update -y && sudo apt-get install -y curl gnupg2 ca-certificates unzip
+sudo apt-get update -y && sudo apt-get install -y curl gnupg2 ca-certificates unzip
 elif [[ $(command -v yum) ]]; then
-
-    sudo yum update -y && sudo yum install -y curl gnupg2 ca-certificates unzip
+sudo yum update -y && sudo yum install -y curl gnupg2 ca-certificates unzip
 elif [[ $(command -v dnf) ]]; then
-
-    sudo dnf update -y && sudo dnf install -y curl gnupg2 ca-certificates unzip
+sudo dnf update -y && sudo dnf install -y curl gnupg2 ca-certificates unzip
 else
     echo "不支持的操作系统" && exit 1
 fi
